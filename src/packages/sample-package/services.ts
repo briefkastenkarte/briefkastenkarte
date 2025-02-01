622b05595e3bc2628a995b6919a5af256ae597d2
Bierett, Danilo <bierett@briefkastenkarte.de>
2025-02-01 06:55:16 +0100

    Remove Open Pioneer sample-package

--- src/packages/sample-package/services.ts
+++
@@ -1,4 +0,0 @@
-// SPDX-FileCopyrightText: 2023 Open Pioneer project (https://github.com/open-pioneer)
-// SPDX-FileCopyrightText: 2025 Briefkastenkarte project (https://github.com/briefkastenkarte-de)
-// SPDX-License-Identifier: Apache-2.0
-export * from "./GreeterImpl";
